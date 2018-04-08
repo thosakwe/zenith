c22080100e31b52ab73948f075cad1b065470d26
Tobe O <thosakwe@gmail.com>
2018-04-08 12:14:15 -0400

    Switch to CancelableOperation for Asset

--- zenith/lib/src/assets/asset.dart
+++ zenith/lib/src/assets/asset.dart
@@ -4,14 +4,20 @@ import 'asset_loader.dart';
 
 /// A piece of data produced by an expensive computation.
 /// 
-/// Internally, this class uses an [AsyncMemoizer] to ensure
+/// Internally, this class performs memoization to ensure
 /// that the computation only is ever run once.
 abstract class Asset<T> {
-  final AsyncMemoizer<T> _memoizer = new AsyncMemoizer();
+  T _value;
 
   /// Gets the value of this asset, either through computation or a memoized value.
-  Future<T> load(AssetLoader loader) => _memoizer.runOnce(() => loadFresh(loader));
+  CancelableOperation<T> load(AssetLoader loader) {
+    if (_value != null) {
+      return new CancelableOperation.fromFuture(new Future.value(_value));
+    }
 
-  /// Loads the value. This is the function called by the [AsyncMemoizer].
-  FutureOr<T> loadFresh(AssetLoader loader);
+    return loadFresh(loader);
+  }
+
+  /// Loads the value. This is the function called by the memoizer.
+  CancelableOperation<T> loadFresh(AssetLoader loader);
 }
